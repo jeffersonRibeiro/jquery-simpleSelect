5268d2ffd3c460c0c9474d7115e1140a883d21b0
jefferson Ribeiro <jefferson.r.silva@infracommerce.com.br>
2018-03-14 11:58:12 -0300

    configurado dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,36 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifyjs = require('gulp-minify');
+
+var paths = {
+  scss: './src/sass/**/*.scss',
+  js: './src/js/*.js'
+}
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src(paths.scss)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./src/css'));
 });
+
+gulp.task('sass:compress', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('js:compress', function () {
+  return gulp.src(paths.js)
+    .pipe(minifyjs())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('deploy', ['sass:compress', 'js:compress']);
  
 gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch(paths.scss, ['sass']);
 });
